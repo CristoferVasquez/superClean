54356a985cf1a82107ce7709ebd5f016b1621ba2
Yessika Arrieta <41095626+Yarrieta@users.noreply.github.com>
2021-03-06 00:51:13 -0300

    Add files via upload
    js con validaciones.

---
+++ validaciones.js
@@ -0,0 +1,19 @@
+   function validate() {
+    
+    var rut = document.getElementById('rut').value;
+    if( rut.length == 0|| isNaN(rut) ) {
+       alert( "El rut indicado es incorrecto!" );
+       document.formularioContacto.rut.focus() ;
+       return false;
+    }
+
+    var dv = document.getElementById('dv').value;
+    if( dv.length == 0|| isNaN(dv) ) {
+      alert( "El digito verificador indicado es incorrecto!" );
+      document.formularioContacto.rut.focus() ;
+      return false;
+   }
+   
+
+    return( true );
+ }
