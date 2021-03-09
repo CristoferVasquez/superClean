9ee1583a3e92c5150bb4acce367f8d888fc25659
CristoferVasquez <cristofervasquezalave@gmail.com>
2021-03-09 02:02:28 -0300

    Update controlador.js

--- js/controlador.js
+++ js/controlador.js
@@ -3,37 +3,39 @@
 function verificar(){
 
     let c = new Cliente();
-   
-        c.rut = document.getElementById('rut').value;
-
-        if (c.rut.length != 0) {
-            let res = c.validarRutCliente();
-            if (res == true) {
-                rut.setCustomValidity("RUT valido"); return true; 
-            }else{
-            rut.setCustomValidity("RUT Inválido"); return false; 
-            }
-        }else{
-            alert("ingrese rut");
-        }
-        alert(res+c.rut);
-      /*  c.nombre = document.getElementById('nombre').value;
-        c.apellido = document.getElementById('apellido').value;
-        c.direccion = document.getElementById('direccion').value
-*/
-
-      /*  let res2 = c.validarText(c.nombre);
-        let res3 = c.validarText(c.apellido);
-
-        alert(res+"-"+res2)
-       
-      */
-
-        
+   try {
+    c.rut = document.getElementById('rut').value;
     
- 
+    c.direccion = document.getElementById('direccion').value
+
+    if (c.rut.length != 0) {
+        let res = c.validarRutCliente();
+        if (res == true) {
+            rut.setCustomValidity("RUT valido"); return true; 
+        }else{
+        rut.setCustomValidity("RUT Inválido"); return false; 
+        }
+    }else{
+        rut.setCustomValidity("Favor ingrese su rut"); return false; 
+    }
+    
+    c.nombre = document.getElementById('nombre').value;
+    c.apellido = document.getElementById('apellido').value;
+    let res2 = c.validarText(c.nombre);
 
 
+    if (condition) {
+        
+    }
+
+    
+
+   } catch (error) {
+       
+   }
+
+
+      
 
 }
 
