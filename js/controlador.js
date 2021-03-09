2948e15c52b88980a4aa4a257991ec678a8f6de2
CristoferVasquez <cristofervasquezalave@gmail.com>
2021-03-08 23:57:52 -0300

    Update controlador.js

--- js/controlador.js
+++ js/controlador.js
@@ -2,12 +2,13 @@ function verificar(){
 
     let c = new Cliente();
     let res = c.validarRutCliente();
-
     if(res==true){
         alert("rut valido")
     }else{
         alert("rut invalido"+c.rut)
     }
+
+
 
 }
 
@@ -15,6 +16,50 @@ function imprimir(){
 
 }
 
+    function sololetras(e) {
+        key=e.keyCode || e.which;
+ 
+        teclado=String.fromCharCode(key).toLowerCase();
+ 
+        letras="qwertyuiopasdfghjklñzxcvbnm ";
+ 
+        especiales="8-37-38-46-164";
+ 
+        teclado_especial=false;
+ 
+        for(var i in especiales){
+            if(key==especiales[i]){
+                teclado_especial=true;
+                break;
+            }
+        }
+ 
+        if(letras.indexOf(teclado)==-1 && !teclado_especial){
+            return false;
+        }
+    }
 
+    function sololetras(e) {
+        key=e.keyCode || e.which;
+ 
+        teclado=String.fromCharCode(key).toLowerCase();
+ 
+        letras="qwertyuiopasdfghjklñzxcvbnm ";
+ 
+        especiales="8-37-38-46-164";
+ 
+        teclado_especial=false;
+ 
+        for(var i in especiales){
+            if(key==especiales[i]){
+                teclado_especial=true;
+                break;
+            }
+        }
+ 
+        if(letras.indexOf(teclado)==-1 && !teclado_especial){
+            return false;
+        }
+    }
 
 
