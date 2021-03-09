6a4db7ecd35a87df234d99079c9fa4b20995138a
CristoferVasquez <cristofervasquezalave@gmail.com>
2021-03-09 01:15:41 -0300

    Update controlador.js

--- js/controlador.js
+++ js/controlador.js
@@ -3,27 +3,29 @@
 function verificar(){
 
     let c = new Cliente();
-    try {
-
+   
         c.rut = document.getElementById('rut').value;
-        c.nombre = document.getElementById('nombre').value;
+        if (c.rut.length != 0) {
+            let res = c.validarRutCliente();
+            alert(res+c.rut);
+        }else{
+            alert("ingrese rut");
+        }
+      /*  c.nombre = document.getElementById('nombre').value;
         c.apellido = document.getElementById('apellido').value;
-        /*c.direccion = document.getElementById('direccion').value
+        c.direccion = document.getElementById('direccion').value
 */
 
-        let res = c.validarRutCliente();
-        let res2 = c.validarText(c.nombre);
+      /*  let res2 = c.validarText(c.nombre);
         let res3 = c.validarText(c.apellido);
-        let res4 = c.validarDire(c.direccion);
 
         alert(res+"-"+res2)
        
-      
-    
-    } catch (error) {
+      */
 
-       alert(error);
-    }
+        
+    
+ 
 
 
 
