5fb76d0bba987a15cf8174b77e56ec93061b4d20
CristoferVasquez <cristofervasquezalave@gmail.com>
2021-03-09 01:08:05 -0300

    Update controlador.js

--- js/controlador.js
+++ js/controlador.js
@@ -4,11 +4,13 @@ function verificar(){
 
     let c = new Cliente();
     try {
+
         c.rut = document.getElementById('rut').value;
         c.nombre = document.getElementById('nombre').value;
         c.apellido = document.getElementById('apellido').value;
-        c.direccion = document.getElementById('direccion').value;
-    
+        /*c.direccion = document.getElementById('direccion').value
+*/
+
         let res = c.validarRutCliente();
         let res2 = c.validarText(c.nombre);
         let res3 = c.validarText(c.apellido);
