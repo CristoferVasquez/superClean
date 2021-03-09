49abb4be4c8f7aee21926473daad14e9b1649421
CristoferVasquez <cristofervasquezalave@gmail.com>
2021-03-09 02:11:01 -0300

    Update controlador.js

--- js/controlador.js
+++ js/controlador.js
@@ -1,27 +1,30 @@
 
 
-function verificar(){
+function crearClientes(){
 
     let c = new Cliente();
    try {
     c.rut = document.getElementById('rut').value;
-    
+    c.nombre = document.getElementById('nombre').value;
+    c.apellido = document.getElementById('apellido').value;
     c.direccion = document.getElementById('direccion').value
 
-    if (c.rut.length != 0) {
-        let res = c.validarRutCliente();
+    let res = c.validarRutCliente();
+    let res2 = c.validarText();
+
+    if (c.rut.length == 0) {
+        
+        rut.setCustomValidity("Favor ingrese su rut"); return false;
+       
+    }else{
         if (res == true) {
             rut.setCustomValidity("RUT valido"); return true; 
         }else{
         rut.setCustomValidity("RUT Inválido"); return false; 
         }
-    }else{
-        rut.setCustomValidity("Favor ingrese su rut"); return false; 
     }
     
-    c.nombre = document.getElementById('nombre').value;
-    c.apellido = document.getElementById('apellido').value;
-    let res2 = c.validarText(c.nombre);
+    
 
 
     if (condition) {
