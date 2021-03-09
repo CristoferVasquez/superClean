cce0935aa93e1723ca25c171d2989adf8e1458fa
CristoferVasquez <cristofervasquezalave@gmail.com>
2021-03-09 02:22:20 -0300

    Update controlador.js

--- js/controlador.js
+++ js/controlador.js
@@ -1,27 +1,7 @@
 
 
-function crearClientes(){
+function verificarCliente(rut){
 
-    let c = new Cliente();
-   try {
-    c.rut = document.getElementById('rut').value;
-    c.nombre = document.getElementById('nombre').value;
-    c.apellido = document.getElementById('apellido').value;
-    c.direccion = document.getElementById('direccion').value
-
-    let res = c.validarRutCliente();
-    let res2 = c.validarText();
-
-   if (res2 == true) {
-    rut.setCustomValidity("Nombre correcto"); return true;
-       
-   }
-
-    
-
-   } catch (error) {
-       
-   }
 
 
       
@@ -29,6 +9,18 @@ function crearClientes(){
 }
 
 function imprimir(){
+    let c = new Cliente();
+    try {
+
+        c.rut = document.getElementById('rut').value;
+        c.nombre = document.getElementById('nombre').value;
+        c.apellido = document.getElementById('apellido').value;
+        c.direccion = document.getElementById('direccion').value;
+
+
+    } catch (error) {
+        
+    }
 
     alert("estoy imprimiendo")
 }
