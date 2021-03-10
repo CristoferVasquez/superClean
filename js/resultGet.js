1fb88499c0c971c1cae0cb4c4c7a5027685874dc
CristoferVasquez <cristofervasquezalave@gmail.com>
2021-03-09 21:53:13 -0300

    Create resultGet.js

---
+++ js/resultGet.js
@@ -0,0 +1,10 @@
+var rut = getParameterByName('rut');
+var nombre = getParameterByName('nombre');
+var apellido = getParameterByName('apellido');
+var direccion = getParameterByName('direccion');
+var tipoVehiculo = getParameterByName('tipoVehiculo');
+var marca = getParameterByName('marca');
+var año = getParameterByName('año');
+var encargado = getParameterByName('encargado');
+var servicio = getParameterByName('comuna');
+
