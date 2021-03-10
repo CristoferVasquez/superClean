5cd0e45d786d592636d5d47a5e2a11d0daa709d5
CristoferVasquez <cristofervasquezalave@gmail.com>
2021-03-09 22:20:49 -0300

    Update resultGet.js

--- js/resultGet.js
+++ js/resultGet.js
@@ -1,10 +1,29 @@
-var rut = getParameterByName('rut');
-var nombre = getParameterByName('nombre');
-var apellido = getParameterByName('apellido');
-var direccion = getParameterByName('direccion');
-var tipoVehiculo = getParameterByName('tipoVehiculo');
-var marca = getParameterByName('marca');
-var año = getParameterByName('año');
-var encargado = getParameterByName('encargado');
-var servicio = getParameterByName('comuna');
+
+function imprimirBoleta(){
+    const valores = window.location.search;
+    const urlParams = new URLSearchParams(valores);
+  
+    var rut = urlParams.get('rut');
+    var nombre = urlParams.get('nombre');
+    var apellido = urlParams.get('apellido');
+    var direccion = urlParams.get('direccion');
+    var tipoVehiculo = urlParams.get('tipoVehiculo');
+    var marca = urlParams.get('marca');
+    var año = urlParams.get('año');
+    var encargado = urlParams.get('encargado');
+    var servicio = urlParams.get('servicio');
+   
+   
+    var total = parseInt(servicio) + (servicio*0.19);
+
+    let texto = "Superclean\nBoleta Servicio de limpieza "+
+                    "\nCliente\nRut:"+rut+"\nNombre Completo: "+nombre+
+                    " "+apellido+"\nDireccion: "+direccion+"\n-----------------------------"+
+                    "\nVehiculo\Tipo de vehiculo: "+tipoVehiculo+"\nMarca: "+marca+
+                    "\nAño de vehiculo: "+año+"\n----------------------------------\nServicios y costo"+
+                    "\nEncargado: "+encargado
+                    +"\nPrecio a pagar "+total+" IVA Incluido";
+
+    console.log(texto);
+}
 
